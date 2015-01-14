0b4b87fef90066671e234c5368e62ea8d6a77c95
Lewis Cowper <lewis.cowper@wandisco.com>
2015-01-14 23:26:00 +0000

    finalised planets endpoint, began the long journey toward having an api wrapper

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # swapico-scheme
 Scheme library for the Star Wars API (http://swapi.co/)
+
+At the moment only the planet endpoint is defined, I'm going to make the whole thing a bit more generic and then I'll create (resource id) style methods for getById style functionality, and then (resource) for get all, and (resource-schema) for getting the JSON schema of the endpoint.
+
+Once that is done, I'll move on to adding validation that (resource) belongs to (endpoints), and then I'll get to look in on how error handling works.
--- swapi.scm
+++ swapi.scm
@@ -2,17 +2,42 @@
 
 (client-software '(("scheme-swapi" #f #f)))
 
+;; Not sure why I can't add the /api
+
+(define baseuri "http://swapi.co/")
+
+;; going to use this when I get to validating that something that I'm making is a valid request
+
+(define (endpoints)
+  (let* ((swapiuri (update-uri (uri-reference baseuri)
+                                path: `(/ "api")))
+         (req (make-request
+               method: 'GET
+               uri: swapiuri)))
+    (with-input-from-request req #f read-json)))
+
+;; this should all be inside a get all method
+
 (define (planets)
     (with-input-from-request "http://swapi.co/api/planets/" #f read-json))
 
-(define (tatooine)
-    (with-input-from-request "http://swapi.co/api/planets/1" #f read-json))
+;; this should be in a get by id method
 
 (define (planet x)
-  (let* ((planetx (number->string x))
-         (planeturi (update-uri (uri-reference "http://swapi.co/")
-                                 path: `(/ "api" "planets" ,planetx)))
+  (let* ((y (->string x))
+         (swapiuri (update-uri (uri-reference baseuri)
+                                 path: `(/ "api" "planets" ,y)))
          (req (make-request
                method: 'GET
-               uri: planeturi)))
+               uri: swapiuri)))
+    (with-input-from-request req #f read-json)))
+
+;; this will hopefully become a generic *-schema method
+
+(define (planet-schema)
+  (let* ((swapiuri (update-uri (uri-reference baseuri)
+                                 path: `(/ "api" "planets" "schema")))
+         (req (make-request
+               method: 'GET
+               uri: swapiuri)))
     (with-input-from-request req #f read-json)))
