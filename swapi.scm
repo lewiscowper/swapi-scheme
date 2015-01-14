3347e83ac1d4c79d0976320fad3b62ed6a53ec03
Lewis Cowper <lewis.cowper@wandisco.com>
2015-01-14 18:36:01 +0000

    First commit with almost working code

---
+++ swapi.scm
@@ -0,0 +1,18 @@
+(require-extension http-client medea intarweb uri-common)
+
+(client-software '(("scheme-swapi" #f #f)))
+
+(define (planets)
+    (with-input-from-request "http://swapi.co/api/planets/" #f read-json))
+
+(define (tatooine)
+    (with-input-from-request "http://swapi.co/api/planets/1" #f read-json))
+
+(define (planet x)
+  (let* ((planetx (number->string x))
+         (planeturi (update-uri (uri-reference "http://swapi.co/")
+                                 path: `(/ "api" "planets" ,planetx)))
+         (req (make-request
+               method: 'Get
+               uri: planeturi)))
+    (with-input-from-request req #f read-json)))
